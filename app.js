9cb5f8aee267c7c9c843429b85b339457ac38b16
jawed <jawedalam5130@gmail.com>
2024-12-01 21:26:49 +0530

    Created Qr Function

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ let qrImg = document.getElementById("qrImg");
 
 let qrText = document.getElementById("qrText");
 
+function generateQr() {
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+}
